4f275f3447d32fec521c69d288b5d378e439a824
silva.rodrigo@icen.ufpa.br <silva.rodrigo@icen.ufpa.br>
2023-04-15 09:55:04 -0300

    feat: iniciando questão 2

--- src/main/java/edu/ufpa/regexp/validator/RegexValidatorQ2.java
+++ src/main/java/edu/ufpa/regexp/validator/RegexValidatorQ2.java
@@ -1,4 +1,11 @@
 package edu.ufpa.regexp.validator;
 
+import lombok.RequiredArgsConstructor;
+
+import java.util.regex.Pattern;
+
+@RequiredArgsConstructor
 public class RegexValidatorQ2 {
+
+    private final Pattern pattern1 = Pattern.compile("[(HM)(MH)][([hm]*m[hm]*m[hm]*)([hm]*h[hm]*)((?=m*hm*h[hm]*)[hm]{3}[hm]*)]]");
 }
